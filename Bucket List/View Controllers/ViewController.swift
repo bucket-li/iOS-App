d5cbf531d83a37b6a610b21cb9542970b4cb2986
Michael Stoffer <mstoffer@michaelstoffer.com>
2019-08-21 21:03:51 -0400

    Initial Commit

---
+++ Bucket List/View Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bucket List
+//
+//  Created by Michael Stoffer on 8/20/19.
+//  Copyright © 2019 Michael Stoffer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
