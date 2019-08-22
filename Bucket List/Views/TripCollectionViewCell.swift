e42ac81b6b97998b858a0a80e4144f886bf53351
Robert Keffury <bobby.keffury@me.com>
2019-08-21 20:04:38 -0700

    Added all features in the storyboard, created files for view controllers and collectionViewCell, and linked them up accordingly.

---
+++ Bucket List/Views/TripCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  TripCollectionViewCell.swift
+//  Bucket List
+//
+//  Created by Bobby Keffury on 8/21/19.
+//  Copyright © 2019 Michael Stoffer. All rights reserved.
+//
+
+import UIKit
+
+class TripCollectionViewCell: UICollectionViewCell {
+    
+}
